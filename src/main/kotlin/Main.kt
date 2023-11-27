081541d495f0bd31b29cecea3a553d3d48ec9b10
FBagdeli <f.bagdeli13@outlook.com>
2023-11-27 21:33:41 +0100

    final
    I write a method that can reserve singed numbers .
    also if the number became more than capacity of Int type, then program will return 0, for this ability i used Long type variables .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,35 @@
+
 fun main() {
-    
+    println( reserveNumbers(123))
+    println( reserveNumbers(-123))
+    println( reserveNumbers(120))
+    println(reserveNumbers(2147483647))
+    println(reserveNumbers(-2147483647))
 }
+
+fun reserveNumbers(x: Int): Int {
+    val maxValue: Int = 2147483647
+    val minValue: Int = -2147483647
+    var newX = x
+    val reservedNumber : Long
+
+    if (newX<0){
+        newX *= -1
+        val stringX = newX.toString()
+        reservedNumber = (stringX.reversed().toLong() * -1)
+
+        return if (reservedNumber in (minValue + 1)..<maxValue) {
+            reservedNumber.toInt()
+        } else 0
+    }else{
+        val stringX = x.toString()
+        reservedNumber = stringX.reversed().toLong()
+
+        return if (reservedNumber in (minValue + 1)..<maxValue) {
+            reservedNumber.toInt()
+        } else 0
+
+    }
+}
+
+
