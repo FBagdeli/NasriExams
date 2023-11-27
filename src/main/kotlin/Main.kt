3abe0e5e29e028e34259de3ef3aabac372ad386b
FBagdeli <f.bagdeli13@outlook.com>
2023-11-28 00:19:48 +0100

    final
    in this program we try to get length of last word of sentence.
    also i added some more codes which we will see the last word of the sentences too!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
+import kotlin.system.exitProcess
+
 fun main() {
-    
+
+    println("\t"+ lastWord("Hello World"))
+    println("\t"+ lastWord("   fly me   to   the moon "))
+    println("\t"+ lastWord("luffy is still joyboy"))
+}
+
+fun lastWord(string: String) : Int {
+
+    val newString = string.reversed()
+    var i = 0
+    if (newString[0] == ' ') {
+        while (newString[i + 1] != ' ') {
+            i++
+        }
+        for (a in i downTo 1) {
+            print(newString[a])
+        }
+    } else {
+        while (newString[i] != ' ') {
+            i++
+        }
+        for (a in i downTo 0) {
+            print(newString[a])
+        }
+    }
+    return i
 }
