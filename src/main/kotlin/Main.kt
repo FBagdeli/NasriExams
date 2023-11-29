929fddd564151ff617492d5ef4bae86b4f3ba576
FBagdeli <f.bagdeli13@outlook.com>
2023-11-29 20:42:37 +0100

    spaces after the lastword
    in this update, the program is able to recognize the spaces after the last word.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,53 +3,33 @@ import kotlin.system.exitProcess
 fun main() {
 
     println("\t"+ lastWord("Hello World"))
-    println("\t"+ lastWord("   fly me   to   the moon "))
+    println("\t"+ lastWord("   fly me   to   the moon    "))
     println("\t"+ lastWord("luffy is still joyboy"))
 }
 
 fun lastWord(string: String) : Int {
 
     var lengthString = string.length -1
-
     var lengthLastWord = 0
-    if (string[lengthString] == ' ')
-    {
-        while (string[lengthString -1] != ' '){
-            lengthLastWord++
+
+    for (i in 0..lengthString ){
+        if (string[lengthString] == ' '){
             lengthString --
-        }
-    }else{
-        while (string[lengthString] != ' '){
-            lengthLastWord++
-            lengthString --
+            if (string[lengthString] != ' '){
+                while (string[lengthString] != ' '){
+                    lengthLastWord++
+                    lengthString --
+                }
+                break
+            }
+        }else{
+            while (string[lengthString] != ' '){
+                lengthLastWord++
+                lengthString --
+            }
+            break
         }
     }
-
     return lengthLastWord
 
-
-
-
-
-
-
-
-//    val newString = string.reversed()
-//    var i = 0
-//    if (newString[0] == ' ') {
-//        while (newString[i + 1] != ' ') {
-//            i++
-//        }
-//        for (a in i downTo 1) {
-//            print(newString[a])
-//        }
-//    } else {
-//        while (newString[i] != ' ') {
-//            i++
-//        }
-//        for (a in i downTo 0) {
-//            print(newString[a])
-//        }
-//    }
-//    return i
 }
